5bfee1ea70213cbcad0c19997ece0128f28ec362
vinni81 <156304876+vinni81@users.noreply.github.com>
2024-01-13 01:31:18 +0300

    Update compare.c

--- compare/compare.c
+++ compare/compare.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 int main() 
-    {
+{
     int num1, num2;
 
     printf("Enter two number whith gap ");
     scanf("%d %d", &num1, &num2);
 
     if (num1 > num2) 
-    {
-        printf("Above\n");
-    } else if (num1 < num2) 
-    {
-        printf("Less\n");
-    } else 
-    {
-        printf("Equal\n");
-    }
-
+    printf("Above\n");
+    
+    else if (num1 < num2) 
+    printf("Less\n");
+    
+    else 
+    printf("Equal\n");
+   
     return 0;
-    }
+}
