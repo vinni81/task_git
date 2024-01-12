881658ce317b7bddd1a0244acdbe6db4f907e0c1
vinni81 <156304876+vinni81@users.noreply.github.com>
2024-01-13 01:16:24 +0300

    Update sum max min.c

--- sum max min/sum max min.c
+++ sum max min/sum max min.c
@@ -15,7 +15,8 @@ int main()
     if (num2 > max) 
     {
         max = num2;
-    } else if (num2 < min) 
+    } 
+    else if (num2 < min) 
     {
         min = num2;
     }
@@ -23,21 +24,25 @@ int main()
     if (num3 > max) 
     {
         max = num3;
-    } else if (num3 < min) 
+    } 
+    else if (num3 < min) 
     {
         min = num3;
     }
 
     if (num4 > max) {
         max = num4;
-    } else if (num4 < min) {
+    } 
+    else if (num4 < min) 
+    {
         min = num4;
     }
 
     if (num5 > max) 
     {
         max = num5;
-    } else if (num5 < min) 
+    } 
+    else if (num5 < min) 
     {
         min = num5;
     }
