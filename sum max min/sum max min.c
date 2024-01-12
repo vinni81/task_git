46bab1b78a2dcdcc54b22cd88cf6f49d2bfef1c7
vinni81 <156304876+vinni81@users.noreply.github.com>
2024-01-13 01:22:16 +0300

    Update sum max min.c

--- sum max min/sum max min.c
+++ sum max min/sum max min.c
@@ -8,49 +8,34 @@ int main()
     printf("Enter five intgers wiht gaps: ");
     scanf("%d %d %d %d %d", &num1, &num2, &num3, &num4, &num5);
 
-  
     max = num1;
     min = num1;
 
     if (num2 > max) 
-    {
-        max = num2;
-    } 
+    max = num2;
+    
     else if (num2 < min) 
-    {
-        min = num2;
-    }
-
+    min = num2;
+    
     if (num3 > max) 
-    {
-        max = num3;
-    } 
+    max = num3;
+   
     else if (num3 < min) 
-    {
-        min = num3;
-    }
+    min = num3;  
 
     if (num4 > max) 
-    {
-        max = num4;
-    } 
-    else if (num4 < min) 
-    {
-        min = num4;
-    }
-
-    if (num5 > max) 
-    {
-        max = num5;
-    } 
-    else if (num5 < min) 
-    {
-        min = num5;
-    }
-
-    
-    sum = max + min;
+    max = num4;
   
+    else if (num4 < min) 
+    min = num4;
+   
+    if (num5 > max) 
+    max = num5;
+    
+    else if (num5 < min) 
+    min = num5;
+   
+    sum = max + min;
     printf(" %d\n", sum);
 
     return 0;
