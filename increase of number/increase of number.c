6c2235c28a8cb6a0fba994021dbfa3ebd1016fda
vinni81 <156304876+vinni81@users.noreply.github.com>
2024-01-13 01:13:19 +0300

    Update increase of number.c

--- increase of number/increase of number.c
+++ increase of number/increase of number.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+    {
     int num1, num2, num3;
 
     printf("enter tree numbers with gaps: ");
     scanf("%d %d %d", &num1, &num2, &num3);
 
-    if (num1 < num2 && num2 < num3) {
+    if (num1 < num2 && num2 < num3) 
+    {
         printf("Yes\n");
-    } else {
+    } else 
+    {
         printf("No\n");
     }
 
     return 0;
-}
+    }
